649896a33782520a3b7a601005f5fe5277ac671e
Timur Kartaev <timurkartaev@gmail.com>
2019-01-14 15:20:23 +0200

    added main app and changed settings

--- gameshop/urls.py
+++ gameshop/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main.views import IndexPageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', IndexPageView.as_view(), name='index_page_view')
 ]
