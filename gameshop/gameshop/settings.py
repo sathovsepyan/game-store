f4bbab3a856c161ce63f801e4719c98d6deb1d36
Timur Kartaev <timurkartaev@gmail.com>
2019-01-14 14:56:31 +0200

    added heroku configurations

--- gameshop/gameshop/settings.py
+++ gameshop/gameshop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
