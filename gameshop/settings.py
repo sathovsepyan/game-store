0a98f6072337e441a4065d9f9314984b5a4d9eec
Timur Kartaev <timurkartaev@gmail.com>
2019-02-18 22:39:38 +0300

    settings fix

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -133,8 +133,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, "static")
 ]
 
 MEDIA_URL = '/media/'
